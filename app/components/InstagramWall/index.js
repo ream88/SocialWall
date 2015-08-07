e5261a7f6862e6da301fd2c25e21a3985533db10
Mario Uher <uher.mario@gmail.com>
2015-08-07 08:45:30 +0200

    InstagramWall shows the 5 lastest images.

--- app/components/InstagramWall/index.js
+++ app/components/InstagramWall/index.js
@@ -1,18 +1,38 @@
 import React, { Component, PropTypes } from 'react';
+import _ from 'lodash';
 
 
 export default class InstagramWall extends Component {
   static propTypes = {
-    images: PropTypes.array
+    images: PropTypes.array.isRequired
+  }
+
+
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      images: []
+    }
+  }
+
+
+  componentWillReceiveProps(props) {
+    let { images } = props;
+
+    images = _.sortByOrder(images, 'created_time', 'desc');
+    images = _.take(images, 5);
+
+    this.setState({ images });
   }
 
 
   render() {
-    if(this.props.images.length == 0) return <ul></ul>;
+    if(this.state.images.length == 0) return <ul></ul>;
 
     return (
       <ul>
-        {this.props.images.map(image =>
+        {this.state.images.map(image =>
           <li key={image.id}>
             <img src={image.images.thumbnail.url} />
           </li>
